eaf9c2a9a8f866aca8cb8760806ac1fc4d511f48
Piotrek Okoński <piotrek@okonski.org>
2011-12-22 13:43:43 +0100

    Settings and front-end

--- bootstrapr.rb
+++ bootstrapr.rb
@@ -4,17 +4,19 @@ class Bootstrapr < Sinatra::Base
 
   # Actions
   get '/' do
-    haml :index
+    haml :index, :locals => {available: available}
   end
 
-  get '/pack/?:compress?' do
-    compress = params[:compress] == "compressed"
+  get '/pack' do
+    compress = params[:compress] == "1"
+    comments = params[:comments] == "1"
     headers "Content-Disposition" => "attachment; filename=bootstrap.#{"min." if compress}js"
     content_type "text/javascript"
-    files = params[:files] ? params[:files].split(',').collect{|f| f if available.include?(f)}.compact : []
+    params[:files] = params[:files].split(",") if params[:files].class.name == "String"
+    files = params[:files] ? params[:files].collect{|f| f if available.include?(f)}.compact : []
 
     content = []
-    ugl = Uglifier.new(copyright: !compress)
+    ugl = Uglifier.new(copyright: comments)
     for file in files
       content << ugl.compile(File.open(File.join(settings.root,"src","bootstrap-#{file}.js")))
     end
